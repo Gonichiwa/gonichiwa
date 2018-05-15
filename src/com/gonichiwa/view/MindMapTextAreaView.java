3a1fa0d9a8c3b5911771d618f0d485a99f90a8ee
penubo <penubokim@gmail.com>
2018-05-15 23:18:29 +0900

    MindMapTextAreaView and test first commit

---
+++ src/com/gonichiwa/view/MindMapTextAreaView.java
@@ -0,0 +1,54 @@
+package com.gonichiwa.view;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class MindMapTextAreaView extends JPanel {
+	
+	JScrollPane textAreaScrollPane;
+	JTextArea mindMapBuildingTextArea;
+	JButton textApplyToModelButton;
+	
+	public MindMapTextAreaView() {
+		this.setLayout(new BorderLayout());
+		
+		initSubViews();
+		this.add(textAreaScrollPane, BorderLayout.CENTER);
+		this.add(textApplyToModelButton, BorderLayout.SOUTH);
+	}
+	
+	public void initSubViews() {
+		mindMapBuildingTextArea = new JTextArea();
+		mindMapBuildingTextArea.setTabSize(1);
+		textAreaScrollPane = new JScrollPane(mindMapBuildingTextArea);
+		textApplyToModelButton = new JButton("APPLY");
+	}
+	
+	public void setFont() {
+		//TODO: study font
+	}
+	
+	public String getText() {
+		return mindMapBuildingTextArea.getText();
+	}
+	
+	public boolean isEmpty() {
+		return mindMapBuildingTextArea.getText().isEmpty();
+	}
+	
+	public void setText(String str) {
+		mindMapBuildingTextArea.setText(null);
+		mindMapBuildingTextArea.setText(str);
+	}
+	
+	public void addApplyButtonListener(ActionListener l) {
+		textApplyToModelButton.addActionListener(l);
+	}
+	
+}
---
+++ src/test/view/MindMapTextAreaViewTest.java
@@ -0,0 +1,56 @@
+package test.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import com.gonichiwa.model.MindMapModel;
+import com.gonichiwa.view.MindMapTextAreaView;
+
+public class MindMapTextAreaViewTest extends JFrame {
+
+	MindMapTextAreaView view;
+	String resultText;
+	MindMapModel model = new MindMapModel();
+	
+	public MindMapTextAreaViewTest() {
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		initSubView();
+		view.addApplyButtonListener(new ApplyButtonListener());
+		
+		this.add(view);
+		
+		this.setSize(800, 600);
+		this.setVisible(true);
+	}
+	
+	public void initSubView() {
+		view = new MindMapTextAreaView();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MindMapTextAreaViewTest();
+	}
+	
+	/*
+	 * must add to controller
+	 */
+	class ApplyButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			resultText = view.getText();
+			try {
+				model.buildNewTree(resultText);
+			} catch(IllegalArgumentException modelerr) {
+				System.out.println(modelerr.getMessage());
+			}
+			System.out.println(model.getTreeSize());
+		}
+		
+	}
+
+}
